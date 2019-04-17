da9959e50d0b54a9d0634966beddfe62e1dd6d00
Charles Ma <charlesma@r-40-103-25-172.comp.nus.edu.sg>
2019-04-17 17:53:48 +0800

    add ravel

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def train(model_name, num_frames=48, num_features=4, saved_model=None,
             y_pred = (y_pred > 0.5)
 
             # tn, fp, fn, tp
-            tn1,fp1,fn1,tp1 = confusion_matrix(y[test], y_pred)
+            tn1,fp1,fn1,tp1 = confusion_matrix(y[test], y_pred).ravel()
             tn += tn1
             fp += fp1
             fn += fn1
